83304c9bf2d734943c342dd471c1d022ba716b77
kristoferjoseph <kristoferjoseph@gmail.com>
2013-11-06 15:12:47 -0800

    Adding import test *that should be failing btw

--- test/tpct.test.js
+++ test/tpct.test.js
@@ -13,4 +13,24 @@ describe('tpct', function() {
             expected = read('test/expected/tpct.expected.css', 'utf-8').toString();
         assert(actual, expected, 'Generated output should match expected file');
     });
+
+    it('should throw error when no input file is supplied', function() {
+        assert.throws(function() {
+            tpct({
+                src: '',
+                dest: 'tmp/tpct.css'
+            });
+        }, Error);
+    });
+
+    it('should import from node_modules', function() {
+        tpct({
+            src: 'test/fixtures/tpct.import.test.css',
+            dest: 'tmp/tpct.import.css'
+        });
+
+        var actual = read('tmp/tpct.import.css'),
+            expected = read('test/expected/tpct.import.expected.css', 'utf-8').toString();
+        assert(actual, expected, 'Generated output should match expected file');
+    });
 });
